4f3719b07d031cea2fda5517308899ce19a40b5a
YeisonTapia <yftapial@ut.edu.co>
2020-05-14 20:57:28 -0500

    Add implementation for findbyId in Agent Service, with TDD

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -1,19 +1,63 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
-const config = {
-  logging: function () {
+const agentFixtures = require('./fixtures/agent')
 
-  }
+let config = {
+  logging () {}
 }
+
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let sinlge = Object.assign({}, agentFixtures.single)
+let id = 1
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDataBase = require('../')
-  db = await setupDataBase(config)
+  sandbox = sinon.createSandbox()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
+  db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called one')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+  t.deepEqual(agent, agentFixtures.byId(id), 'Should be the same')
 })
